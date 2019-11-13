dd71e77259ddb609f4b721484c871a13a52af9b9
Masaaki Yana <nativeyana@gmail.com>
2019-11-13 15:00:17 -0800

    room.py: Updated __init__() to accept items, added docstring for class Room, started interactive method while loop

--- room.py
+++ room.py
@@ -1,16 +1,47 @@
 '''
 room.py
 
-Class
+Class Room
 -----
-
-Room
+properties:
+    location
+    items
+    description
 '''
-class Room:
 
-    def __init__(self, unit_name, direction_of_doors=[]):
+available_items = []
+items_kept_by_user = []
+possible_verbs = ['take', 'drop', 'use', 'go']
+
+
+class Room:
+    '''
+    Create a room or garden    
+    parameters for instantiation
+        unit_name: str
+        room_info: str
+        items: list
+        direction_of_doors: list
+    '''
+
+    def __init__(self, unit_name, room_info, items=[], direction_of_doors=[]):
         self.unit_name = unit_name
+        self.room_info = room_info
+        self.unit_items = items
         self.direction_of_doors = direction_of_doors
+        global available_items
+        available_items += self.unit_items
+
+    def _print_properties(self):
+        '''
+        print properties
+        '''
+        print(f"""
+Location: {self.unit_name}
+Doors: {self.direction_of_doors} 
+Items: {self.unit_items}
+Description: {self.room_info}
+""")
 
     def add_next_room(self, next_room, door_dirction):
         '''
@@ -25,8 +56,20 @@ class Room:
         '''
         # 1. print room name, list of doors(direction), list of item
         # 2. Ask what wnat to do "go which direction" or "take an item" or drop an item"
-        # 3. Return ansers 
-        pass
+        # 3. Return an answer
+        print(f"You are in location: {self.unit_name} ")
+        print(f"   You have choices actions: either 'verb item' or 'go to a next location'")
+        print(f"   Available verbs: [take, drop, use, go]")
+        print(f'   Example of answer: "take knife" or "go west"')
+        print("Available items: ", ', '.join([ item for item in self.unit_items ]))
+        print("Available directions: ", ', '.join([ door for door in self.direction_of_doors ]))
+
+        self.answer = 'default'
+        while len(self.answer.split()) != 2 or self.answer.split()[0] not in possible_verbs:
+            self.answer = input("What is your action?['verb item' or 'go direction']? ")
+        self.verb, self.x = self.answer.split()
+        print("Your answer is: ", self.verb, self.x)
+
 
     def go_to_next_place(self, next_place):
         '''
@@ -36,3 +79,5 @@ class Room:
         return:
             the next place's instance? 
         '''
+        pass
+
