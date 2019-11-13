153945291bdaea8fead9c85f037c71be3388ae23
Masaaki Yana <nativeyana@gmail.com>
2019-11-12 18:23:46 -0800

    Initial: room.py and start.py

---
+++ start.py
@@ -0,0 +1,12 @@
+'''
+start.py
+
+Game start and controlled
+'''
+# 1. Import room module
+# 2. Create multiple rooms( Enterance, Living, Kitchen, Bathroom, etc)
+#    - Each room unit has one to four doors to north/sourth/west/east
+#    - Each room unit has some items, hummer, knife, key, candle 
+#    - Person can do action take or drop
+#    - Only if the person has a proper key, secret door can be opend for a treasure
+# 3. Connect them 
